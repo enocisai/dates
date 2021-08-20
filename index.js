277d10a9532f13adae7aa16baadf7d37b0d4335f
Enoc I. Jaramillo Castro <enocisaicastro@gmail.com>
2021-08-20 15:26:54 -0700

    single-line approach

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-function dateParser(oldDate){
+const dateParser = oldDate=>`${oldDate.split("-")[1]}/${oldDate.split("-")[2]}/${oldDate.split("-")[0]}/`
 
-  //Lets transform a string into an array 
-  const date = oldDate.split("-")
-  
-  // Create a new array 
-  const array = [date[1], date [2], date [0]]
-  
-  // Add a separator between months and year
-  const finalDate = array.join("/");
-
-  return finalDate
-  
-}
-const result = dateParser("2020-10-05");
-console.log(result)
+console.log(dateParser("1995-19-05"))
